3009f664f2d4f5cb282827866c796283bbf59cb2
jagaldol <yoy06056@pusan.ac.kr>
2023-09-20 22:08:55 +0900

    feat: Response 양식 통일을 위한 Wrapper 클래스(ApiUtils) 생성

---
+++ src/main/java/com/bungaebowling/server/_core/utils/ApiUtils.java
@@ -0,0 +1,28 @@
+package com.bungaebowling.server._core.utils;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiUtils {
+
+    public static <T> Response<T> success(T response) {
+        return new Response<>(HttpStatus.OK.value(), response, null);
+    }
+
+    public static <T> Response<T> success() {
+        return new Response<>(HttpStatus.OK.value(), null, null);
+    }
+
+    public static <T> Response<T> error(String errorMessage, HttpStatus status) {
+        return new Response<>(status.value(), null, errorMessage);
+    }
+    public static <T> Response<T> error(String errorMessage, Integer status) {
+        return new Response<>(status, null, errorMessage);
+    }
+
+    public record Response<T>(
+            int status,
+            T response,
+            String errorMessage
+    ) {}
+
+}
