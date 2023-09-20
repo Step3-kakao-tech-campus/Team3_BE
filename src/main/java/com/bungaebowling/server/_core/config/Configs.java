adcce3a8ebe7486404d742c66e0e2a1700956981
jagaldol <yoy06056@pusan.ac.kr>
2023-09-21 03:41:10 +0900

    fix: CORS에 "*" 제거
    - 리프레시 토큰의 경우 Cookie로 전달하기 때문에 allowCredentials=true로 설정함
    - 이 경우 "*" 를 사용하여 전체 도메인에 대한 허용을 할 수 없음
    - 따라서 리액트 포트인 3000포트의 localhost와 127.0.0.1만을 임시로 등록

--- src/main/java/com/bungaebowling/server/_core/config/Configs.java
+++ src/main/java/com/bungaebowling/server/_core/config/Configs.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class Configs {
     public final static List<String> CORS = Collections.unmodifiableList(
             List.of("http://localhost:3000", // 리액트 개발용 3000포트
-                        "*")  // 모든 IP 주소 허용 (나중에 프론트 앤드 IP만 허용하게 바꿔야함)
+                    "http://127.0.0.1:3000")
     );
 }
