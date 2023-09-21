24d28b91b474bc78174ca78671f9b431363552e6
jagaldol <yoy06056@pusan.ac.kr>
2023-09-22 02:42:56 +0900

    feat: GET /posts mock 구현

--- src/main/java/com/bungaebowling/server/post/controller/PostController.java
+++ src/main/java/com/bungaebowling/server/post/controller/PostController.java
@@ -1,12 +1,66 @@
 package com.bungaebowling.server.post.controller;
 
+import com.bungaebowling.server._core.utils.ApiUtils;
+import com.bungaebowling.server._core.utils.cursor.CursorRequest;
+import com.bungaebowling.server.post.dto.PostResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        CursorRequest cursorRequest = new CursorRequest(1L, 20);
+        List<PostResponse.GetPostsDto.PostDto> postDtos = new ArrayList<>();
+        PostResponse.GetPostsDto.PostDto postDto1 = new PostResponse.GetPostsDto.PostDto(
+                1L,
+                "오늘 7시에 부산대 락볼링장에서 게임하실분~~",
+                "9월 7일 (목) 오후 9:00",
+                "부산광역시 금정구 장전2동",
+                "9월 9일 (토) 오전 9:00",
+                0,
+                "김볼링",
+                null,
+                false
+        );
+        postDtos.add(postDto1);
+        PostResponse.GetPostsDto.PostDto postDto2 = new PostResponse.GetPostsDto.PostDto(
+                2L,
+                "오늘 당장 나올사람!",
+                "9월 7일 (목) 오후 8:00",
+                "부산광역시 동래구",
+                "9월 7일 (목) 오후 8:30",
+                0,
+                "최볼링",
+                null,
+                false
+        );
+        postDtos.add(postDto2);
+        PostResponse.GetPostsDto.PostDto postDto3 = new PostResponse.GetPostsDto.PostDto(
+                3L,
+                "오늘 7시에 부산대 락볼링장에서 게임하실분~~",
+                "9월 7일 (목) 오후 3:00",
+                "부산광역시 부산진구 부전동",
+                "9월 8일 (금) 오후 7:00",
+                0,
+                "이볼링",
+                null,
+                false
+        );
+        postDtos.add(postDto3);
+
+        PostResponse.GetPostsDto getPostsDto = new PostResponse.GetPostsDto(cursorRequest, postDtos);
+
+        ApiUtils.Response<?> response = ApiUtils.success(getPostsDto);
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/com/bungaebowling/server/post/dto/PostResponse.java
+++ src/main/java/com/bungaebowling/server/post/dto/PostResponse.java
@@ -1,4 +1,27 @@
 package com.bungaebowling.server.post.dto;
 
+import com.bungaebowling.server._core.utils.cursor.CursorRequest;
+
+import java.util.List;
+
 public class PostResponse {
+
+    public record GetPostsDto(
+        CursorRequest nextCursorRequest,
+        List<PostDto> posts
+    ) {
+        public record PostDto(
+                Long id,
+                String title,
+                String dueTime,
+                String districtName,
+                String startTime,
+                Integer averageScore,
+                String userName,
+                String profileImage,
+                Boolean isClose
+        ) {
+        }
+
+    }
 }
