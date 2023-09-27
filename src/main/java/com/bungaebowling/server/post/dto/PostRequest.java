7965f4f2cc1eea0e23da63e0c126ede5ef3e2125
sososo0 <sh0000@pusan.ac.kr>
2023-09-27 20:46:55 +0900

    feat: Add PostRequestDTO
    - PostCreateRequestDTO and PostUpdateRequestDTO
    - Separate CreateDTO and UpdateDTO because of potential API spec changes
      in the future

--- src/main/java/com/bungaebowling/server/post/dto/PostRequest.java
+++ src/main/java/com/bungaebowling/server/post/dto/PostRequest.java
@@ -1,4 +1,67 @@
 package com.bungaebowling.server.post.dto;
 
+import com.bungaebowling.server.post.Post;
+import com.bungaebowling.server.user.User;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
 public class PostRequest {
+
+    public record PostCreateRequest (
+        @NotBlank(message = "모집글 제목은 필수 입력 사항입니다.")
+        @Size(max = 100, message = "모집글 제목은 최대 100자까지 입니다.")
+        String title,
+
+        @NotNull(message = "게임 예정 일시는 필수 입력 사항입니다.")
+        LocalDateTime startTime,
+
+        @NotNull(message = "모집 마감 기한은 필수 입력 사항입니다.")
+        LocalDateTime dueTime,
+
+        @NotNull(message = "모집글 내용은 필수 입력 사항입니다.")
+        String content,
+        boolean isClose
+    ) {
+        public Post toEntity(User user) {
+            return Post.builder()
+                    .user(user)
+                    .title(title)
+                    .startTime(startTime)
+                    .dueTime(dueTime)
+                    .content(content)
+                    .isClose(isClose)
+                    .build();
+        }
+    }
+
+    public record PostUpdateRequest (
+            @NotBlank(message = "모집글 제목은 필수 입력 사항입니다.")
+            @Size(max = 100, message = "모집글 제목은 최대 100자까지 입니다.")
+            String title,
+
+            @NotNull(message = "게임 예정 일시는 필수 입력 사항입니다.")
+            LocalDateTime startTime,
+
+            @NotNull(message = "모집 마감 기한은 필수 입력 사항입니다.")
+            LocalDateTime dueTime,
+
+            @NotNull(message = "모집글 내용은 필수 입력 사항입니다.")
+            String content,
+            boolean isClose
+    ) {
+        public Post toEntity(User user) {
+            return Post.builder()
+                    .user(user)
+                    .title(title)
+                    .startTime(startTime)
+                    .dueTime(dueTime)
+                    .content(content)
+                    .isClose(isClose)
+                    .build();
+        }
+    }
 }
