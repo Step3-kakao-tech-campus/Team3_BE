0507ac6f10618cbdf512c7525eed0294f0c8bdfd
jagaldol <yoy06056@pusan.ac.kr>
2023-09-30 19:54:01 +0900

    feat: 상태 상관없이 전체 목록 리턴
    아직 수락 안된 신청자 목록만 리턴 시,
    수락된 신청자들의 목록을 확인할 추가적인 api가 필요.
    현재 구성에서는 수락된 신청자의 목록도 리턴하는게 나음

--- src/main/java/com/bungaebowling/server/applicant/repository/ApplicantRepository.java
+++ src/main/java/com/bungaebowling/server/applicant/repository/ApplicantRepository.java
@@ -16,10 +16,10 @@ public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
     @Query("SELECT a FROM Applicant a WHERE a.user.id = :userId AND a.post.id = :postId")
     Optional<Applicant> findByUserIdAndPostId(@Param("userId") Long userId, @Param("postId") Long postId);
 
-    @Query("SELECT a FROM Applicant a JOIN FETCH a.user u WHERE a.post.id = :postId AND a.status = false ORDER BY a.id DESC")
+    @Query("SELECT a FROM Applicant a JOIN FETCH a.user u WHERE a.post.id = :postId ORDER BY a.id DESC")
     List<Applicant> findAllByPostIdOrderByIdDesc(@Param("postId") Long postId, Pageable pageable);
 
-    @Query("SELECT a FROM Applicant a JOIN FETCH a.user u WHERE a.post.id = :postId AND a.id < :key AND a.status = false ORDER BY a.id DESC")
+    @Query("SELECT a FROM Applicant a JOIN FETCH a.user u WHERE a.post.id = :postId AND a.id < :key ORDER BY a.id DESC")
     List<Applicant> findAllByPostIdLessThanOrderByIdDesc(@Param("key") Long key, @Param("postId") Long postId, Pageable pageable);
 
     @Query("SELECT count(a) FROM Applicant a WHERE a.post.id = :postId AND a.status = true")
