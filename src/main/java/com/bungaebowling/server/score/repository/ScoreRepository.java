26ee562e7dec2eee48dedc1e751fc419d972176c
sososo0 <sh0000@pusan.ac.kr>
2023-10-07 07:20:28 +0900

    feat: Add findAllByPostId in repository

--- src/main/java/com/bungaebowling/server/score/repository/ScoreRepository.java
+++ src/main/java/com/bungaebowling/server/score/repository/ScoreRepository.java
@@ -4,6 +4,10 @@ import com.bungaebowling.server.score.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
+
+    List<Score> findAllByPostId(Long postId);
 }
