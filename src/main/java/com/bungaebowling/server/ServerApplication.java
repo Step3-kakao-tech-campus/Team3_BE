704672eae9877e748406aba69364cd2e20fe5f5c
sososo0 <sh0000@pusan.ac.kr>
2023-10-07 23:05:34 +0900

    fix: endpoint error

--- src/main/java/com/bungaebowling/server/ServerApplication.java
+++ src/main/java/com/bungaebowling/server/ServerApplication.java
@@ -14,6 +14,10 @@ public class ServerApplication {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
+	//  EC2 인스턴스가 아닌 다른곳에서 해당 애플리케이션을 실행할 때 추가해야 함
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
