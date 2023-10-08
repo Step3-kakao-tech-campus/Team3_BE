ed95bcfd5f172c6a27c1d557062e7d3f663c94dd
yunzae <dsa05037@naver.com>
2023-10-08 19:00:27 +0900

    fix: 쪽지 읽음 상태 기능 수정
    조회한 메시지만 읽음 처리 -> 조회 api 호출시 상대방과의 모든 메시지 읽음 처리

--- src/main/java/com/bungaebowling/server/message/service/MessageService.java
+++ src/main/java/com/bungaebowling/server/message/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.bungaebowling.server.message.service;
 import com.bungaebowling.server._core.errors.exception.client.Exception400;
+import com.bungaebowling.server._core.errors.exception.client.Exception404;
 import com.bungaebowling.server._core.utils.CursorRequest;
 import com.bungaebowling.server.message.Message;
 import com.bungaebowling.server.message.dto.MessageRequest;
@@ -7,6 +8,7 @@ import com.bungaebowling.server.message.dto.MessageResponse;
 import com.bungaebowling.server.message.repository.MessageRepository;
 import com.bungaebowling.server.user.User;
 import com.bungaebowling.server.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,13 +26,13 @@ public class MessageService {
     private final UserRepository userRepository;
     public static final int DEFAULT_SIZE= 20;
 
-    public MessageResponse.GetOpponentsDto getOpponents(CursorRequest cursorRequest, Long opponentId) {
+    public MessageResponse.GetOpponentsDto getOpponents(CursorRequest cursorRequest, Long userId) {
         int size = cursorRequest.hasSize() ? cursorRequest.size() : DEFAULT_SIZE;
         Pageable pageable = PageRequest.of(0, size);
-        userRepository.findById(opponentId).orElseThrow(()->new Exception400("존재하지 않는 유저입니다."));
-        List<Message> messages = messageRepository.findLatestMessagesPerOpponentByUserId(opponentId, cursorRequest.key(), pageable);
-        Long countNew = messageRepository.countByUserIdAndIsReceiveTrueAndIsReadFalse(opponentId);
-        Long countAll = messageRepository.countByUserIdAndIsReceiveTrue(opponentId);
+        userRepository.findById(userId).orElseThrow(()->new Exception404("존재하지 않는 유저입니다."));
+        List<Message> messages = messageRepository.findLatestMessagesPerOpponentByUserId(userId, cursorRequest.key(), pageable);
+        Long countNew = messageRepository.countByUserIdAndIsReceiveTrueAndIsReadFalse(userId);
+        Long countAll = messageRepository.countByUserIdAndIsReceiveTrue(userId);
         Long lastKey = messages.isEmpty() ? CursorRequest.NONE_KEY : messages.get(messages.size()-1).getId();
         return MessageResponse.GetOpponentsDto.of(cursorRequest.next(lastKey, DEFAULT_SIZE),countNew,countAll,messages);
     }
@@ -39,20 +41,23 @@ public class MessageService {
     @Transactional
     public MessageResponse.GetMessagesDto getMessagesByOpponentId(CursorRequest cursorRequest, Long userId, Long opponentId) {
         if (userId.equals(opponentId)){throw new Exception400("본인과 쪽지 대화를 할 수 없습니다.");}
+        User opponentUser = userRepository.findById(opponentId).orElseThrow(()->new Exception404("존재하지 않는 유저입니다."));
+        // 벌크 업데이트 쿼리 - 작업 후 영속성 초기화 실행
+        messageRepository.updateIsReadByUserIdAndOpponentUserId(userId,opponentId);
+
         int size = cursorRequest.hasSize() ? cursorRequest.size() : DEFAULT_SIZE;
         Pageable pageable = PageRequest.of(0, size);
-        User opponentUser = userRepository.findById(opponentId).orElseThrow(()->new Exception400("존재하지 않는 유저입니다."));
         List<Message> messages= messageRepository.findAllByUserIdAndOpponentUserIdOrderByIdDesc(userId,opponentId,cursorRequest.key(), pageable);
-        messages.stream().forEach(m-> m.read());
         Long lastKey = messages.isEmpty() ? CursorRequest.NONE_KEY : messages.get(messages.size()-1).getId();
+
         return MessageResponse.GetMessagesDto.of(cursorRequest.next(lastKey, DEFAULT_SIZE),messages,opponentUser);
     }
 
     @Transactional
     public void sendMessage(MessageRequest.SendMessageDto requestDto, Long userId, Long opponentId) {
         if (userId.equals(opponentId)){throw new Exception400("본인과 쪽지 대화를 할 수 없습니다.");}
-        User user = userRepository.findById(userId).orElseThrow(()->new Exception400("존재하지 않는 유저입니다."));
-        User opponentUser = userRepository.findById(opponentId).orElseThrow(()->new Exception400("존재하지 않는 유저입니다."));
+        User user = userRepository.findById(userId).orElseThrow(()->new Exception404("존재하지 않는 유저입니다."));
+        User opponentUser = userRepository.findById(opponentId).orElseThrow(()->new Exception404("존재하지 않는 유저입니다."));
         Message userMessage = requestDto.toEntity(user,opponentUser,false,true);
         Message opponentMessage = requestDto.toEntity(opponentUser,user,true,false);
         messageRepository.save(userMessage);
@@ -61,13 +66,13 @@ public class MessageService {
 
     @Transactional
     public void deleteMessagesByOpponentId(Long userId, Long opponentId) {
-        userRepository.findById(userId).orElseThrow(()->new Exception400("존재하지 않는 유저입니다."));
+        userRepository.findById(userId).orElseThrow(()->new Exception404("존재하지 않는 유저입니다."));
         messageRepository.deleteAllByUserIdAndOpponentUserId(userId,opponentId);
     }
     
     @Transactional
     public void deleteMessageById(Long userId, Long messageId) {
-        Message message = messageRepository.findById(messageId).orElseThrow(()-> new Exception400("존재하지 않는 쪽지입니다."));
+        Message message = messageRepository.findById(messageId).orElseThrow(()-> new Exception404("존재하지 않는 쪽지입니다."));
         if (!userId.equals(message.getUser().getId())){
             throw new Exception400("해당 유저의 쪽지가 아닙니다.");
         }
