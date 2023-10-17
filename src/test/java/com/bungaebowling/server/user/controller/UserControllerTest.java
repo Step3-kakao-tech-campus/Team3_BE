5bb146354f6db5a68e0aa2c5edb90ec65e522821
jagaldol <yoy06056@pusan.ac.kr>
2023-10-17 19:20:37 +0900

    test: Create Success Test of /join

--- src/test/java/com/bungaebowling/server/user/controller/UserControllerTest.java
+++ src/test/java/com/bungaebowling/server/user/controller/UserControllerTest.java
@@ -1,15 +1,71 @@
 package com.bungaebowling.server.user.controller;
 
+import com.bungaebowling.server.user.dto.UserRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @MockBean(name = "redisTemplate")
+    private RedisTemplate<String, String> redisTemplate;
+
+    @MockBean
+    private RedisKeyValueAdapter redisKeyValueAdapter;
+
+    @Mock
+    private ValueOperations<String, String> valueOperations;
+
     @Test
-    void join() {
+    @DisplayName("회원가입 테스트 - 성공")
+    void join() throws Exception {
+        // given
+        UserRequest.JoinDto joinDto = new UserRequest.JoinDto("testCode회원", "testCode@test.com", "testCode12!@", "1");
+
+        String requestBody = om.writeValueAsString(joinDto);
+
+        BDDMockito.given(redisTemplate.opsForValue()).willReturn(valueOperations);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        // then
+        var responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        Object json = om.readValue(responseBody, Object.class);
+        System.out.println("[response]\n" + om.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+
+        resultActions.andExpectAll(
+                status().isOk(),
+                jsonPath("$.status").value(200)
+        );
     }
 
     @Test
