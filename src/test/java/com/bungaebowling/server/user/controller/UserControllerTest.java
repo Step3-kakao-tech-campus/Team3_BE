c65a689d677d473ec3ce5432925ba0b8e52b872f
jagaldol <yoy06056@pusan.ac.kr>
2023-10-17 19:19:48 +0900

    test: Create UserControllerTest

---
+++ src/test/java/com/bungaebowling/server/user/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.bungaebowling.server.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class UserControllerTest {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void reIssueTokens() {
+    }
+
+    @Test
+    void sendVerification() {
+    }
+
+    @Test
+    void confirmEmail() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getMyProfile() {
+    }
+
+    @Test
+    void updateMyProfile() {
+    }
+
+    @Test
+    void getUserRecords() {
+    }
+}
