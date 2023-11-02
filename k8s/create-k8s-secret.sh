a38df284fe53c8c0f0acd7bfa16c2985fafd10e9
sososo0 <sh0000@pusan.ac.kr>
2023-11-02 14:03:50 +0900

    fix: Add endpoint in secret sh file

--- k8s/create-k8s-secret.sh
+++ k8s/create-k8s-secret.sh
@@ -13,6 +13,7 @@ kubectl create secret generic $SECRET_NAME \
   --from-literal=TOKEN_SECRET="$TOKEN_SECRET" \
   --from-literal=GMAIL_USERNAME="$GMAIL_USERNAME" \
   --from-literal=GMAIL_APPLICATION_PASSWORD="$GMAIL_APPLICATION_PASSWORD" \
+  --from-literal=AWS_S3_END_POINT="$AWS_S3_END_POINT" \
   --from-literal=AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
   --from-literal=AWS_SECRET_KEY="$AWS_SECRET_KEY" \
   --from-literal=MYSQL_USERNAME="$MYSQL_USERNAME" \
