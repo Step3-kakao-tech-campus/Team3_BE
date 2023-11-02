a85b9a45f6bb74f43972eca6c2ddcee96431af9f
jagaldol <yoy06056@pusan.ac.kr>
2023-11-02 14:09:47 +0900

    docs: Create api docs of getComments

--- src/test/java/com/bungaebowling/server/comment/controller/CommentControllerTest.java
+++ src/test/java/com/bungaebowling/server/comment/controller/CommentControllerTest.java
@@ -1,6 +1,12 @@
 package com.bungaebowling.server.comment.controller;
 
+import com.bungaebowling.server.ApiTag;
 import com.bungaebowling.server.ControllerTestConfig;
+import com.bungaebowling.server.GeneralApiResponseSchema;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
+import com.epages.restdocs.apispec.SimpleType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +20,11 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -64,6 +74,43 @@ class CommentControllerTest extends ControllerTestConfig {
                 jsonPath("$.response.comments[*].childComments[*].content", everyItem(notNullValue())),
                 jsonPath("$.response.comments[*].childComments[*].createdAt", everyItem(notNullValue())),
                 jsonPath("$.response.comments[*].childComments[*].editedAt", everyItem(notNullValue()))
+        ).andDo(
+                MockMvcRestDocumentationWrapper.document(
+                        "[comment] getComments",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .summary("댓글 조회")
+                                        .description("""
+                                                모집글의 댓글들을 조회합니다.
+                                                """)
+                                        .tag(ApiTag.COMMENT.getTagName())
+                                        .pathParameters(parameterWithName("postId").description("조회할 댓글들의 모집글 id"))
+                                        .responseSchema(Schema.schema("댓글 조회 응답 DTO"))
+                                        .responseFields(
+                                                GeneralApiResponseSchema.NEXT_CURSOR.getResponseDescriptor().and(
+                                                        fieldWithPath("response.comments[].id").description("댓글의 ID(PK)"),
+                                                        fieldWithPath("response.comments").description("댓글"),
+                                                        fieldWithPath("response.comments[].userId").optional().type(SimpleType.NUMBER).description("댓글 작성자의 ID(PK) | 삭제된 댓글의 경우 null"),
+                                                        fieldWithPath("response.comments[].userName").optional().type(SimpleType.STRING).description("댓글 작성자의 닉네임 | 삭제된 댓글의 경우 null"),
+                                                        fieldWithPath("response.comments[].profileImage").optional().type(SimpleType.STRING).description("댓글 작성자 프로필 이미지 경로"),
+                                                        fieldWithPath("response.comments[].content").description("댓글 내용 | 삭제된 댓글의 경우 '삭제된 댓글입니다.'"),
+                                                        fieldWithPath("response.comments[].createdAt").description("댓글 생성 시간"),
+                                                        fieldWithPath("response.comments[].editedAt").description("댓글 마지막 수정 시간"),
+                                                        fieldWithPath("response.comments[].childComments").description("대댓글"),
+                                                        fieldWithPath("response.comments[].childComments[].id").description("대댓글 ID"),
+                                                        fieldWithPath("response.comments[].childComments[].userId").description("대댓글 작성자의 ID(PK)"),
+                                                        fieldWithPath("response.comments[].childComments[].userName").description("대댓글 작성자의 닉네임"),
+                                                        fieldWithPath("response.comments[].childComments[].profileImage").description("대댓글 작성자 프로필 이미지 경로"),
+                                                        fieldWithPath("response.comments[].childComments[].content").description("대댓글 내용"),
+                                                        fieldWithPath("response.comments[].childComments[].createdAt").description("대댓글 생성 시간"),
+                                                        fieldWithPath("response.comments[].childComments[].editedAt").description("대댓글 마지막 수정 시간")
+                                                )
+                                        )
+                                        .build()
+                        )
+                )
         );
     }
 
