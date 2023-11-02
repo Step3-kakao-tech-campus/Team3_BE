d429a32c8d4f2e31a3c1bf7582c4f370b2113dba
jagaldol <yoy06056@pusan.ac.kr>
2023-11-02 13:02:08 +0900

    test: Create test of getComments

--- src/test/java/com/bungaebowling/server/comment/controller/CommentControllerTest.java
+++ src/test/java/com/bungaebowling/server/comment/controller/CommentControllerTest.java
@@ -7,10 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @ActiveProfiles(value = {"test"})
@@ -26,6 +32,39 @@ class CommentControllerTest extends ControllerTestConfig {
     @Test
     @DisplayName("댓글 조회 테스트")
     void getComments() throws Exception {
+        // given
+        Long postId = 1L;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                RestDocumentationRequestBuilders
+                        .get("/api/posts/{postId}/comments", postId)
+        );
+        // then
+        var responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        Object json = om.readValue(responseBody, Object.class);
+        System.out.println("[response]\n" + om.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+
+
+        resultActions.andExpectAll(
+                status().isOk(),
+                jsonPath("$.status").value(200),
+                jsonPath("$.response.nextCursorRequest").exists(),
+                jsonPath("$.response.comments[*].id", everyItem(isA(Integer.class))),
+                jsonPath("$.response.comments[*].userId").hasJsonPath(),
+                jsonPath("$.response.comments[*].userName").hasJsonPath(),
+                jsonPath("$.response.comments[*].profileImage").hasJsonPath(),
+                jsonPath("$.response.comments[*].content", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].createdAt", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].editedAt", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].childComments[*].id", everyItem(isA(Integer.class))),
+                jsonPath("$.response.comments[*].childComments[*].userId", everyItem(isA(Integer.class))),
+                jsonPath("$.response.comments[*].childComments[*].userName", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].childComments[*].profileImage").hasJsonPath(),
+                jsonPath("$.response.comments[*].childComments[*].content", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].childComments[*].createdAt", everyItem(notNullValue())),
+                jsonPath("$.response.comments[*].childComments[*].editedAt", everyItem(notNullValue()))
+        );
     }
 
     @Test
