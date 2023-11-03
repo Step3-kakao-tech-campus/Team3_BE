60c7f841d982880471857d95149f06f8eb04c81b
yunzae <dsa05037@naver.com>
2023-11-04 06:32:57 +0900

    test: Add message test - deleteMessagesByOpponentId
    쪽지함 삭제 테스트를 추가하였습니다.

--- src/test/java/com/bungaebowling/server/message/controller/MessageControllerTest.java
+++ src/test/java/com/bungaebowling/server/message/controller/MessageControllerTest.java
@@ -137,5 +137,35 @@ public class MessageControllerTest extends ControllerTestConfig {
         );
     }
 
+    @Test
+    @DisplayName("쪽지함 삭제 테스트")
+    void deleteMessagesByOpponentId() throws Exception {
+        // given
+        Long userId = 1L;
+        Long opponentId = 3L;
+        String accessToken = JwtProvider.createAccess(
+                User.builder()
+                        .id(userId)
+                        .role(Role.ROLE_USER)
+                        .build()
+        ); // 김볼링
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                RestDocumentationRequestBuilders
+                        .delete("/api/messages/opponents/{opponentId}", opponentId)
+                        .header(HttpHeaders.AUTHORIZATION, accessToken)
+        );
+        // then
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        Object json = om.readValue(responseBody, Object.class);
+        System.out.println("[response]\n" + om.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+
+        resultActions.andExpectAll(
+                status().isOk(),
+                jsonPath("$.status").value(200)
+        );
+    }
+
 
 }
