7a0b8b1283e348790fe0ced1304c61cf97807467
sososo0 <sh0000@pusan.ac.kr>
2023-11-04 22:31:32 +0900

    fix: Change field to constructor autowired

--- src/main/java/com/bungaebowling/server/_core/utils/AwsS3Service.java
+++ src/main/java/com/bungaebowling/server/_core/utils/AwsS3Service.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.bungaebowling.server._core.errors.exception.CustomException;
 import com.bungaebowling.server._core.errors.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,11 +21,14 @@ import java.util.List;
 import java.util.Objects;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class AwsS3Service {
-    @Autowired
     private final AmazonS3 amazonS3Client;
+
+    @Autowired
+    public AwsS3Service(AmazonS3 amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
