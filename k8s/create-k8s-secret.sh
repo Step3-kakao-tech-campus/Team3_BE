f17e7f7c116272010aa39dda822e39a779773440
sososo0 <sh0000@pusan.ac.kr>
2023-11-06 11:08:05 +0900

    fix: Change docker image and add mysql root password

--- k8s/create-k8s-secret.sh
+++ k8s/create-k8s-secret.sh
@@ -16,6 +16,7 @@ kubectl create secret generic $SECRET_NAME \
   --from-literal=AWS_S3_END_POINT="$AWS_S3_END_POINT" \
   --from-literal=AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
   --from-literal=AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+  --from-literal=MYSQL_PASSWORD="$MYSQL_ROOT_PASSWORD" \
   --from-literal=MYSQL_USERNAME="$MYSQL_USERNAME" \
   --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" \
   --from-literal=DOMAIN="$DOMAIN" \
