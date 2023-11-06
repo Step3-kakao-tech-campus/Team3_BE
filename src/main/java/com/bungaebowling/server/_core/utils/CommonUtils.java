1ea3361e3d573282be5a4aad5cd6cd70cf35cfdc
xcelxlorx <xcelxlorx@gmail.com>
2023-11-06 22:57:00 +0900

    refactor: Remove duplicate codes

--- src/main/java/com/bungaebowling/server/_core/utils/CommonUtils.java
+++ src/main/java/com/bungaebowling/server/_core/utils/CommonUtils.java
@@ -13,12 +13,7 @@ public class CommonUtils {
 
     // 점수 등록용
     public static String buildScoreFileName(Long userId, Long postId, String category, LocalDateTime time, String originalFileName) {
-        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR); // 파일 확장자 구분선
-
-        if (fileExtensionIndex == -1) {
-            throw new CustomException(ErrorCode.FILE_REQUEST_FAILED);
-        }
-
+        int fileExtensionIndex = getFileExtensionIndex(originalFileName);
         String fileExtension = originalFileName.substring(fileExtensionIndex); // 파일 확장자
         String fileName = originalFileName.substring(0, fileExtensionIndex); // 파일 이름
         String now = String.valueOf(time); // 파일 업로드 시간
@@ -29,18 +24,22 @@ public class CommonUtils {
 
     //프로필 등록
     public static String buildProfileFileName(Long userId, String category, LocalDateTime time, String originalFileName) {
-        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR); // 파일 확장자 구분선
-
-        if (fileExtensionIndex == -1) {
-            throw new CustomException(ErrorCode.FILE_REQUEST_FAILED);
-        }
-
+        int fileExtensionIndex = getFileExtensionIndex(originalFileName);
         String fileExtension = originalFileName.substring(fileExtensionIndex); // 파일 확장자
         String fileName = originalFileName.substring(0, fileExtensionIndex); // 파일 이름
         String now = String.valueOf(time); // 파일 업로드 시간
 
         //작성자(user_1)/profile/파일명/파일업로드시간.확장자
         return "user" + WORD_SEPARATOR + userId + CATEGORY_PREFIX + category + CATEGORY_PREFIX + fileName + TIME_SEPARATOR + now + fileExtension;
+    }
+
+    private static int getFileExtensionIndex(String originalFileName) {
+        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR); // 파일 확장자 구분선
+
+        if (fileExtensionIndex == -1) {
+            throw new CustomException(ErrorCode.FILE_REQUEST_FAILED);
+        }
+        return fileExtensionIndex;
     }
 
     // 단일 파일용 -> 이것도 사용 용도에 맞게 custom해서 써야 함
