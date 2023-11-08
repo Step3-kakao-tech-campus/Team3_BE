71132fb1c5bfd5f77d207b635301af78ec2d056c
sososo0 <sh0000@pusan.ac.kr>
2023-11-08 22:02:33 +0900

    fix: Add mail server env

--- k8s/create-k8s-secret.sh
+++ k8s/create-k8s-secret.sh
@@ -21,7 +21,8 @@ kubectl create secret generic $SECRET_NAME \
   --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" \
   --from-literal=DOMAIN="$DOMAIN" \
   --from-literal=GOOGLE_MAP_API_KEY="$GOOGLE_MAP_API_KEY" \
-  --from-literal=API_SERVER_URL="$API_SERVER_URL"
+  --from-literal=API_SERVER_URL="$API_SERVER_URL" \
+  --from-literal=FLASK_MAIL_SERVER="$FLASK_MAIL_SERVER"
 
 
 echo "Kubernetes secret $SECRET_NAME has been created or updated with the environment variables."
