479eebdc950b247ab28e95a1d9401708b019775e
sososo0 <sh0000@pusan.ac.kr>
2023-11-08 13:40:31 +0900

    fix: Remove frontent env in pod info

--- k8s/create-k8s-secret.sh
+++ k8s/create-k8s-secret.sh
@@ -21,8 +21,7 @@ kubectl create secret generic $SECRET_NAME \
   --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" \
   --from-literal=DOMAIN="$DOMAIN" \
   --from-literal=GOOGLE_MAP_API_KEY="$GOOGLE_MAP_API_KEY" \
-  --from-literal=API_SERVER_URL="$API_SERVER_URL" \
-  --from-literal=NEXT_PUBLIC_KAKAOMAP_APPKEY="$NEXT_PUBLIC_KAKAOMAP_APPKEY"
+  --from-literal=API_SERVER_URL="$API_SERVER_URL"
 
 
 echo "Kubernetes secret $SECRET_NAME has been created or updated with the environment variables."
