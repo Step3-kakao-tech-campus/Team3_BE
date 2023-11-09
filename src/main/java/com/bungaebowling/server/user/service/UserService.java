3db41b11ce04e531169790b6e76c61a194afae79
sososo0 <sh0000@pusan.ac.kr>
2023-11-09 21:44:21 +0900

    fix: Add 5000 port in requestUrl

--- src/main/java/com/bungaebowling/server/user/service/UserService.java
+++ src/main/java/com/bungaebowling/server/user/service/UserService.java
@@ -191,7 +191,7 @@ public class UserService {
             log.info("json: "+ requests);
 
             HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requests, httpHeaders);
-            String requestURL = "http://" + mailServer + "/email";
+            String requestURL = "http://" + mailServer + ":5000/email";
 
             log.info("requestURL: "+ requestURL);
 
